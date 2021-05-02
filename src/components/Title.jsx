01d7e89edb00ead46bf529a270a6c8c4533fe47f
Sarkraf <sarkraf.dev@gmail.com>
2021-05-02 16:35:36 +0200

    change main page

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+
+    return (
+        <>
+          <h1>J'AIME SALAMECHE</h1>
+        </>
+    )
+}
+
+export default Title
---
+++ src/pages/Salameche.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Salameche = () => {
+
+    return (
+      <>
+        <img alt='Salameche' src='https://res.cloudinary.com/teepublic/image/private/s--45IfnNqa--/t_Preview/b_rgb:5e366e,c_limit,f_jpg,h_630,q_90,w_630/v1467160780/production/designs/564425_1.jpg'/>
+      </>  
+    )
+}
+
+export default Salameche
